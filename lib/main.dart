9a1a0d113c92e4edf0fe9ef100b35226f1996316
dc143cDev <devc143c@gmail.com>
2023-02-13 21:27:32 +0900

    git init

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(
     GetMaterialApp(
-      title: "Application",
+      theme: ThemeData(
+        textTheme: GoogleFonts.robotoTextTheme(),
+      ),
+      debugShowCheckedModeBanner: false,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
